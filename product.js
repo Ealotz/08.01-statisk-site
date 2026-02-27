58029069b842ce883aab5668a128017c10f65f0c
Ealotz <ealo1000@stud.ek.dk>
2026-02-27 11:21:43 +0100

    dynamiskproduktliste

--- product.js
+++ product.js
@@ -1,20 +1,48 @@
-const productData = {
-  Navn: "Lyseblå Tshirt",
-  Brand: "NiCe",
-  Farve: "Lyseblå",
-  Pris: "299 kr.",
-  stock: "På lager",
-  billede: "https://kea-alt-del.dk/t7/images/webp/640/1562.webp",
-};
+const id = 1528;
+const productURL = "https://kea-alt-del.dk/t7/api/products/" + id;
+const productcontainer = document.querySelector("#productContainer");
 
-document.querySelector("#productName").innerHTML = productData.produktnavn;
-document.querySelector("#productBrand").innerHTML = productData.brandnavn;
-document.querySelector("#productFarve").innerHTML = productData.produktfarve;
-document.querySelector("#productPris").innerHTML = productData.pris + " kr.";
-document.querySelector("#productImage").innerHTML = productData.image;
-
-if (productData.soldout === 1) {
-  document.querySelector("#productStock").textContent = "Udsolgt";
-} else {
-  document.querySelector("#productStock").textContent = "På lager";
+function getData() {
+  fetch(productURL).then((res) => res.json().then((data) => show(data)));
 }
+
+function show(data) {
+  productcontainer.innerHTML = `
+
+    <div class="produktbillede" id="productImage">
+  <img src="https://kea-alt-del.dk/t7/images/webp/640/${id}.webp" alt="Produkt 2" />
+  </div>
+  
+   <div class="produkttekst">
+
+  <h3 class="nike">${data.productdisplayname}</h3>
+            <h4>${data.brandname}</h4>
+
+            <div class="farve">
+                <p class="overskift">Season:</p>
+                <p class="svar">${data.season}</p>
+             </div>
+
+            <div class="farve">
+                <p class="overskift">Inventory Nummer:</p>
+                <p class="svar">${data.id}</p>
+            </div>
+
+            <div class="farve">
+                <p class="overskift">Price:</p>
+                <p class="svar">${data.price} kr.</p>
+            </div>
+
+
+            <div class="farve">
+            <p class="overskift">Stock:</p>
+             <p class="svar">${data.soldout}</p>
+            </div>
+
+            <button class="basket">Add to Basket</button>
+
+            </div>
+  `;
+}
+
+getData();
