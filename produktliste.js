634c20d3c58004f04a92cf4891cba112ba820a32
Ealotz <ealo1000@stud.ek.dk>
2026-03-05 22:48:10 +0100

    finaleversion

--- produktliste.js
+++ produktliste.js
@@ -1,18 +1,25 @@
 const params = new URLSearchParams(window.location.search);
 const category = params.get("category");
+const pricesbtn = document.querySelector("#sorterPris");
+const filteredmen = document.querySelector("#sortermaend");
+
+console.log(pricesbtn);
 
 const listURL = "https://kea-alt-del.dk/t7/api/products?category=" + category;
 const listContainer = document.querySelector(".product-gallery");
 
 function getProducts() {
-  fetch(listURL).then((res) => res.json().then((products) => showProducts(products)));
+  fetch(listURL).then((res) =>
+    res.json().then((products) => {
+      allProducts = products;
+      showProducts(products);
+    }),
+  );
 }
 
 function showProducts(products) {
-  // Start med tom container
   listContainer.innerHTML = "";
 
-  // products er et array af objekter
   products.forEach((product) => {
     listContainer.innerHTML += `
      <article class="product">
@@ -21,10 +28,31 @@ function showProducts(products) {
                 <p class="price">Price: ${product.price} kr.</p>
                 <p class="price">Brand: ${product.brandname}</p>
                 <p class="price">Stock: ${product.soldout}</p>
-                <a href="produkt.html?id=${product.id}"class="btn">Add to Basket</a>
+                <p class="discount ">${product.discount}%</p>
+                <a href="produkt.html?id=${product.id}"class="btn">Read More</a>
             </article>
     `;
   });
 }
 
 getProducts();
+
+pricesbtn.addEventListener("click", sorterPris);
+
+function sorterPris() {
+  const sorted = [...allProducts].sort((a, b) => a.price - b.price);
+  showProducts(sorted);
+  console.log("tekst");
+}
+
+getProducts();
+
+function sortermaend(targetGender) {
+  console.log("tekst");
+
+  const filtered = allProducts.filter((product) => (product.gender || "").toLowerCase() === targetGender.toLowerCase());
+
+  showProducts(filtered);
+}
+
+filteredmen.addEventListener("click", () => sortermaend("Men"));
